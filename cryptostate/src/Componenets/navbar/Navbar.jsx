d615699d8956ee6f428769f79277c66c7d0843ce
Prateek <singhprateek2704@gmail.com>
2021-10-29 23:45:22 +0530

    Added navbar html

--- cryptostate/src/Componenets/navbar/Navbar.jsx
+++ cryptostate/src/Componenets/navbar/Navbar.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from 'react';
+
+import './Navbar.css';
 
 function Navbar() {
     return (
-        <div>
-            Navbar
+        <div className="navbar">
+            
+            <div className="navbar-container flex-row">
+                <ul className="navbar-items ">
+                    <li className="navbar-item ">Home</li>
+                    <li className="navbar-item ">Properties</li>
+                    <li className="navbar-item ">About Us</li>
+                </ul>
+
+                
+            </div>
+
+            <button className="signup-button flex-row">Sign Up</button>
         </div>
     )
 }
