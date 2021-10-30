aa455de10148da500d016fff184137952328af85
Prateek <singhprateek2704@gmail.com>
2021-10-30 17:15:45 +0530

    Improved navbar

--- cryptostate/src/index.js
+++ cryptostate/src/index.js
@@ -2,21 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-=======
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 import './index.css';
 
 
-
-ReactDOM.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>,
-
-import {BrowserRouter as Router} from 'react-router-dom'
 ReactDOM.render(
   <Router>
       <React.StrictMode>
